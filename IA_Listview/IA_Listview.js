5267bdeb2a2c14abff6c40da8678d91de396bb4f
Nimit_Shah <nimitshah099@gmail.com>
2024-12-10 12:50:26 +0530

    functionality issue was in setting date in examdata function

--- IA_Listview/IA_Listview.js
+++ IA_Listview/IA_Listview.js
@@ -98,13 +98,19 @@ function examData() {
                             }
                         }
                     });
-                    
+
                     // Arranging Data in Variable
                     for (const key of Exammap.keys()) {
+                        let exam_date = new Date(Exammap.get(key).Date);
+                        let year = exam_date.getFullYear();
+                        let month = ('0' + (exam_date.getMonth() + 1)).slice(-2);
+                        let day = ('0' + exam_date.getDate()).slice(-2);
+                        exam_date = `${year}-${month}-${day}`;
+
                         examdata.push({
                             'exam_id': key,
                             'exam_name': Exammap.get(key).Name,
-                            'date': new Date(Exammap.get(key).Date).toISOString().split('T')[0],
+                            'date': exam_date,
                             'Section 1': (Section1.get(key) == undefined ? 0 : Section1.get(key)),
                             'Section 2': (Section2.get(key) == undefined ? 0 : Section2.get(key)),
                             'Section 3': (Section3.get(key) == undefined ? 0 : Section3.get(key)),
