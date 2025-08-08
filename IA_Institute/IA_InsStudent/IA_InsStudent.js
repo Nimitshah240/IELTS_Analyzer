6cf84a50f638cdb889d6d421c04ddc0eb026ea71
NimitShah240 <nimitshah240@gmail.com>
2025-08-08 16:09:13 +0530

    Mostly css work is done, left with backend

--- IA_Institute/IA_InsStudent/IA_InsStudent.js
+++ IA_Institute/IA_InsStudent/IA_InsStudent.js
@@ -1,3 +1,6 @@
+const urlSearchParams = new URLSearchParams(window.location.search);
+let instituteId = urlSearchParams.get('insId');
+
 async function listviewconnectedCallback() {
     try {
         await getEnglishJsonFile('../../CommonUtils/en_properties.json');
@@ -16,6 +19,29 @@ window.addEventListener('message', function (event) {
             if (message.source == 'IA_Delete') {
                 afterDelete(message.data);
             }
+        }
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+function openStudentAddPopup(params) {
+    try {
+        let data = null;
+        document.getElementById('popupFrame').style.display = "flex";
+        popupFrame.contentWindow.postMessage({ source: 'insStudentAddPopup', command: 'openPopup', data: { 'data': data, 'instituteId': instituteId } }, enProperties.domainName);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+window.addEventListener('message', function (event) {
+    try {
+        if (event.origin !== enProperties.domainName) return;
+
+        const message = event.data;
+        if (message.command == 'closePopup') {
+            document.getElementById('popupFrame').style.display = "none";
         }
     } catch (error) {
         console.log(error);
---
+++ IA_Institute/IA_InsStudentAddPopup/IA_InsStudentAddPopup.js
@@ -0,0 +1,91 @@
+let mainData;
+let instituteId;
+document.addEventListener('DOMContentLoaded', function () {
+    window.addEventListener('message', function (event) {
+        try {
+            mainData = event.data.data.data;
+            instituteId = event.data.data.instituteId;
+            setData(mainData);
+            getEnglishJsonFile("../../CommonUtils/en_properties.json");
+        } catch (error) {
+            console.log(error);
+        }
+    });
+});
+
+function closeBtn(params) {
+    popupclose();
+}
+
+function popupclose() {
+    try {
+        parent.postMessage(
+            { source: 'IA_InsStudentAddPopup', command: 'closePopup', data: 'data' },
+            enProperties.domainName)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+async function getSaveBtn(params) {
+    try {
+        let btnText = document.getElementById('btnYes').innerText;
+        if (btnText === 'Get') {
+            email = document.getElementById('email').value;
+            id = document.getElementById('id').value;
+            if (id != null && email != null) {
+                let searchKey = id.trim() != '' ? id : email;
+                apiURL = enProperties.apiURL + enProperties.apiEndPoints.institute + enProperties.apiEndPoints.insStudentData + `?searchKey=${searchKey}`;
+                let responsedata = await apiCallOuts(apiURL, "GET", null, 6000);
+                setData(responsedata);
+                delete mainData.createdDate;
+                delete mainData.updatedDate;
+                delete mainData.createdBy;
+                delete mainData.updatedBy;
+            }
+        } else if (btnText === 'Save') {
+            mainData.instituteId = instituteId;
+            apiURL = enProperties.apiURL + enProperties.apiEndPoints.institute + enProperties.apiEndPoints.insStudentData;            
+            let responsedata = await apiCallOuts(apiURL, "POST", JSON.stringify(mainData), 6000);
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+function setData(data) {
+    try {
+        if (data != null && data.studentId != null) {
+            if (data.studentId != null) {
+                document.getElementById('id').value = data.studentId;
+                document.getElementById('id').disabled = true;
+            }
+            if (data.name != null) document.getElementById('name').value = data.name;
+            if (data.number != null) document.getElementById('number').value = data.number;
+
+            if (data.email != null) {
+                document.getElementById('email').value = data.email;
+                document.getElementById('email').disable = true;
+            }
+            document.getElementById('name-div').style.display = 'flex';
+            document.getElementById('number-div').style.display = 'flex';
+            document.getElementById('orText').style.display = 'none';
+            document.getElementById('btnYes').innerText = 'Save';
+            // data = {'id'}
+            mainData = data;
+            console.log(mainData);
+            
+        }
+        else if (data != null && data.message != null) createToast('error', data.message);
+        else {
+            document.getElementById('btnYes').innerText = 'Get';
+            document.getElementById('id').disabled = false;
+            document.getElementById('email').disabled = false;
+            document.getElementById('name-div').style.display = 'none';
+            document.getElementById('number-div').style.display = 'none';
+            document.getElementById('orText').style.display = 'flex';
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
