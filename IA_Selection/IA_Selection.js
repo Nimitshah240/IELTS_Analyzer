78fe0dc8a5458bf88a176f3293016968a02f8533
Nimit_Shah <nimitshah099@gmail.com>
2025-09-22 01:57:46 +0530

    adding exam system

--- IA_Selection/IA_Selection.js
+++ IA_Selection/IA_Selection.js
@@ -19,6 +19,8 @@ async function selectionconnectedCallback() {
             setTextsImages(0);
         } else if (selectionType == 'trick') {
             setTextsImages(2);
+        } else if (selectionType == 'exams') {
+            setTextsImages(3);
         }
     } catch (error) {
         console.error(error);
